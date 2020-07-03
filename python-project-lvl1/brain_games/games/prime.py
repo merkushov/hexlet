4c7d5f962da3cd74e45f2fef725d92e5331ea0ed
Victor Merkushov <merkushov.victor@gmail.com>
2020-07-03 09:01:32 +0300

    Fourth game: Determine if a number is prime

---
+++ python-project-lvl1/brain_games/games/prime.py
@@ -0,0 +1,34 @@
+"""The module that generates the game according to the given rules"""
+
+from random import randint
+
+TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(n):
+    if (n <= 1):
+        return False
+
+    if (n % 2 == 0):
+        return False
+
+    for i in range(3, n, 2):
+        if (n % i == 0):
+            return False
+
+    return True
+
+
+def get_round():
+    """
+        The function of generating one round of the game.
+        Returns a tuple consisting of 2 elements:
+            - the value for the round of the game
+            - the correct answer in this round
+    """
+    number = randint(1, 1000)
+
+    if is_prime(number):
+        return (str(number), 'yes')
+    else:
+        return (str(number), 'no')
---
+++ python-project-lvl1/brain_games/scripts/brain_prime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import brain_games.engine
+from brain_games.games import prime
+
+
+def main():
+    brain_games.engine.run(prime)
+
+
+if __name__ == "__main__":
+    main()
