299144a1bb510864d1ff96ffe090d18bdf16f0fd
Victor Merkushov <merkushov.victor@gmail.com>
2020-07-09 14:22:58 +0300

    Work with the command line. Parsing Arguments

---
+++ python-project-lvl2/gendiff/cli.py
@@ -0,0 +1,53 @@
+'''The module works with the command line interface'''
+
+import argparse
+
+
+def parse_arguments(version=None):
+    '''
+        Command line argument parsing method.
+
+        Returns the dictionary from the resulting values.
+    '''
+    parser = argparse.ArgumentParser(
+        prog='gendiff',
+        description='Generate difference between two files',
+        allow_abbrev=False
+    )
+    parser.add_argument(
+        '-f',
+        '--format',
+        choices=['dictionary', 'json', 'plain'],
+        default='dictionary',
+        help='Set output format. Default %(default)s',
+        type=str
+    )
+    parser.add_argument(
+        'file1',
+        help='''
+            File number 1 for comparison.
+            Must be in .json or .yaml format.
+        ''',
+        type=argparse.FileType(mode='r', encoding='UTF-8'),
+    )
+    parser.add_argument(
+        'file2',
+        help='''
+            File number 2 for comparison.
+            Must be in .json or .yaml format.
+        ''',
+        type=argparse.FileType(mode='r', encoding='UTF-8'),
+    )
+    parser.add_argument(
+        '--version',
+        action='version',
+        version='%(prog)s {}'.format(version)
+    )
+
+    args = parser.parse_args()
+
+    return {
+        "filepath_1": args.file1.name,
+        "filepath_2": args.file2.name,
+        "format": args.format
+    }
--- python-project-lvl2/gendiff/scripts/gendiff.py
+++ python-project-lvl2/gendiff/scripts/gendiff.py
@@ -2,9 +2,16 @@
 
 """Gendiff script. Application start point"""
 
+import gendiff.cli
+import gendiff.engine
+
 
 def main():
     print("Hello! I'm gendiff")
+    args = gendiff.cli.parse_arguments(
+        version=gendiff.engine.__version__
+    )
+    print(args)
 
 
 if __name__ == "__main__":
