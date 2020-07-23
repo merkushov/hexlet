4d77074ddc15ba35b7b6db1224a8efcd7a5e998f
Victor Merkushov <merkushov.victor@gmail.com>
2020-07-23 10:10:28 +0300

    Added the Dictionary serializer

--- python-project-lvl2/gendiff/engine.py
+++ python-project-lvl2/gendiff/engine.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 import gendiff.deserializer
 import gendiff.serializer
--- python-project-lvl2/gendiff/serializers/dictionary.py
+++ python-project-lvl2/gendiff/serializers/dictionary.py
@@ -6,8 +6,63 @@
       * deleted items are marked with "-"
 """
 
-import json
+from gendiff.config import ADDED, CHANGED, EQUAL, NESTED, REMOVED
+
+INDENT_DEFAULT = 4
 
 
-def process(data):
-    return json.dumps(data, indent=4)
+def process(data, indent=0):
+    """
+        Serializes data structure to a formated dictionary.
+        Recursive function
+    """
+
+    local_indent = (indent + INDENT_DEFAULT) * ' '
+    local_indent_with_sign = (indent + INDENT_DEFAULT - 2) * ' '
+
+    result = '{\n'
+    for key in sorted(data.keys()):
+        string = ''
+        node = data[key]
+
+        if node[0] == ADDED:
+            string = '{}+ "{}":{},\n'.format(
+                local_indent_with_sign,
+                key,
+                node[1]
+            )
+        elif node[0] == CHANGED:
+            string = '{}- "{}":{},\n'.format(
+                local_indent_with_sign,
+                key,
+                node[1][0]
+            )
+            string = string + '{}+ "{}":{},\n'.format(
+                local_indent_with_sign,
+                key,
+                node[1][1]
+            )
+        elif node[0] == EQUAL:
+            string = '{}"{}":{},\n'.format(
+                local_indent,
+                key,
+                node[1]
+            )
+        elif node[0] == NESTED:
+            string = '{}"{}":'.format(
+                local_indent,
+                key
+            )
+            string = string + process(node[1], indent + INDENT_DEFAULT)
+        elif node[0] == REMOVED:
+            string = '{}- "{}":{},\n'.format(
+                local_indent_with_sign,
+                key,
+                node[1]
+            )
+
+        if string:
+            result = result + string
+
+    result = result + (indent * " ") + '}\n'
+    return result
