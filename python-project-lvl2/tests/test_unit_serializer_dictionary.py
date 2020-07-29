e3ef136d769e3a4d16266a7d73b09f48c2345711
Victor Merkushov <merkushov.victor@gmail.com>
2020-07-29 21:22:01 +0300

    Rewrote tests to use pytest.mark.parametrize

--- python-project-lvl2/tests/test_unit_serializer_dictionary.py
+++ python-project-lvl2/tests/test_unit_serializer_dictionary.py
@@ -4,59 +4,46 @@
     and convert them into strings.
 """
 
+import pytest
 import gendiff.serializers.dictionary
 
 
-def _check(data, success_string):
+@pytest.mark.parametrize(
+    'data,success_string', [
+        (
+            {"a": ["added", "new"]},
+            '{\n  + "a":new,\n}\n'
+        ),
+        (
+            {"a": ["changed", ["1", "2"]]},
+            '{\n  - "a":1,\n  + "a":2,\n}\n'
+        ),
+        (
+            {"d": ["equal", 55]},
+            '{\n    "d":55,\n}\n'
+        ),
+        (
+            {"c": ["removed", "old"]},
+            '{\n  - "c":old,\n}\n'
+        ),
+        (
+            {"n": ["nested", {"a": ["added", "new"]}]},
+            '{\n    "n":{\n      + "a":new,\n    }\n}\n'
+        ),
+        (
+            {
+                "n": ["nested", {"a": ["added", "my"]}],
+                "b": ["changed", ["foo", "bar"]],
+                "eq": ["equal", "abc"]
+            },
+            '{\n  - "b":foo,\n  + "b":bar,\n    '
+            + '"eq":abc,\n    "n":{\n      + "a":my,\n    }\n}\n'
+        )
+    ]
+)
+def test_process(data, success_string):
     result_str = gendiff.serializers.dictionary.process(data)
     assert(
         isinstance(result_str, str)
         and result_str == success_string
-    )
-
-
-def test_process_added():
-    _check(
-        {"a": ["added", "new"]},
-        '{\n  + "a":new,\n}\n'
-    )
-
-
-def test_process_changed():
-    _check(
-        {"a": ["changed", ["1", "2"]]},
-        '{\n  - "a":1,\n  + "a":2,\n}\n'
-    )
-
-
-def test_process_equal():
-    _check(
-        {"d": ["equal", 55]},
-        '{\n    "d":55,\n}\n'
-    )
-
-
-def test_process_removed():
-    _check(
-        {"c": ["removed", "old"]},
-        '{\n  - "c":old,\n}\n'
-    )
-
-
-def test_process_nested_added():
-    _check(
-        {"n": ["nested", {"a": ["added", "new"]}]},
-        '{\n    "n":{\n      + "a":new,\n    }\n}\n'
-    )
-
-
-def test_process_multiple_actions():
-    _check(
-        {
-            "n": ["nested", {"a": ["added", "my"]}],
-            "b": ["changed", ["foo", "bar"]],
-            "eq": ["equal", "abc"]
-        },
-        '{\n  - "b":foo,\n  + "b":bar,\n    '
-        + '"eq":abc,\n    "n":{\n      + "a":my,\n    }\n}\n'
     )
